981d0bf0fcab6d0c49cce21ea6d00d75e476d77a
Marek Martofel <mmartofel@users.noreply.github.com>
2018-08-27 17:54:56 +0200

    Update kafka_start.sh

--- kafka_start.sh
+++ kafka_start.sh
@@ -2,8 +2,8 @@
 
 cd /opt/kafka
 
-bin/zookeeper-server-start.sh config/zookeeper.properties &
+bin/zookeeper-server-start.sh config/zookeeper.properties > /opt/kafka/zookeeper_start.log 2>&1 &
 
 sleep 5
 
-bin/kafka-server-start.sh config/server.properties &
+bin/kafka-server-start.sh config/server.properties > /opt/kafka/kafka_start.log 2>&1 &
